04664602286f8b106a4dc5ed18526a297f3e5350
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2026-04-08 16:27:55 +0900

    feat(transform-imports): add e2e example

---
+++ examples/transform-imports/src/mock-lib/button.ts
@@ -0,0 +1,6 @@
+export default function Button() {
+  const el = document.createElement('button')
+  el.textContent = 'Button'
+  el.className = 'mock-button'
+  return el
+}
---
+++ examples/transform-imports/src/mock-lib/card.ts
@@ -0,0 +1,6 @@
+export default function Card() {
+  const el = document.createElement('div')
+  el.textContent = 'Card'
+  el.className = 'mock-card'
+  return el
+}
